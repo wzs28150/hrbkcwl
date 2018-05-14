126d1a7e92f2ae26d4e6f12fad379c93794d7c09
wzs28150 <wz666s@163.com>
2018-05-14 22:43:46 +0800

    add

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
 <div id="page">
-	<div v-swiper:mySwiper="swiperOption"
-	     class="banner">
+	<!-- banner start -->
+	<div v-swiper:mySwiper="swiperOption" class="banner">
 		<div class="swiper-wrapper ">
-			<div class="swiper-slide"
-			     v-for="banner in bannerList"
-			     :key="banner.id">
+			<div class="swiper-slide" v-for="banner in bannerList" :key="banner.id">
 				<!-- <img :src="banner.img"> -->
-				<div class="bg h-center"
-				     :style="{backgroundImage: 'url(' + banner.img + ')'}">
+				<div class="bg h-center" :style="{backgroundImage: 'url(' + banner.img + ')'}">
 					<div class="text-group" data-swiper-parallax="-900">
 						<h2 v-html="banner.title" data-swiper-parallax="-500"></h2>
 						<p data-swiper-parallax="-300">{{banner.description}}</p>
@@ -17,14 +14,13 @@
 				</div>
 			</div>
 		</div>
-		<div class="scroll"><a href="#content"
-			   class="go-down"><img src="~assets/img/go-down.png" alt=""></a></div>
-		<div class="swiper-button-prev swiper-button-white"
-		     slot="button-prev"></div>
-		<div class="swiper-button-next swiper-button-white"
-		     slot="button-next"></div>
+		<div class="scroll"><a href="#content" class="go-down"><img src="~assets/img/go-down.png" alt=""></a></div>
+		<div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
+		<div class="swiper-button-next swiper-button-white" slot="button-next"></div>
 	</div>
-	<div class="i-about">
+	<!-- banner end -->
+	<!-- about start -->
+	<div class="i-about padding-top-100 padding-bottom-100">
 		<div class="container">
 			<el-row :gutter="20">
 				<el-col :span="16">
@@ -35,15 +31,8 @@
 					</div>
 					<!-- Tab -->
 					<ul class="about-tabs">
-						<li :class="[nowTab == item.id ? 'active' : '']"
-						    v-for="item in aboutList"
-						    :key="item.key"
-						    @click="tabToggle(item.id)">
-							<a href="#retina0"
-							   aria-controls="home"
-							   role="tab"
-							   data-toggle="tab"
-							   aria-expanded="false">
+						<li :class="[nowTab == item.id ? 'active' : '']" v-for="item in aboutList" :key="item.key" @click="tabToggle(item.id)">
+							<a href="#retina0" aria-controls="home" role="tab" data-toggle="tab" aria-expanded="false">
 									<i :class="item.icon"></i>
 									{{item.id}}. {{item.title}}
 								</a>
@@ -51,38 +40,43 @@
 					</ul>
 					<!-- Tab panes -->
 					<div class="tab-content">
-						<transition name="fade"
-						            mode="out-in">
-							<div role="tabpanel"
-							     v-for="item in aboutList"
-							     :key="item.id"
-							     v-if="nowTab == item.id"
-							     class="tab-pane">
+						<transition name="fade" mode="out-in">
+							<div role="tabpanel" v-for="item in aboutList" :key="item.id" v-if="nowTab == item.id" class="tab-pane">
 								<p>{{item.content}}</p>
 							</div>
 						</transition>
 					</div>
 				</el-col>
 				<el-col :span="8">
-					<div v-swiper:aboutpicSwiper="swiperOption2"
-					     class="banner aboutpic">
+					<div v-swiper:aboutpicSwiper="swiperOption2" class="aboutpic">
 						<div class="swiper-wrapper ">
-							<div class="swiper-slide"
-							     v-for="banner in bannerList"
-							     :key="banner.id">
+							<div class="swiper-slide" v-for="pic in aboutpic" :key="pic.id">
 								<!-- <img :src="banner.img"> -->
-								<div class="bg h-center"
-								     :style="{backgroundImage: 'url(' + banner.img + ')'}">
+								<div class="bg h-center" :style="{backgroundImage: 'url(' + pic.thumb + ')'}">
 								</div>
 							</div>
 						</div>
+						<div class="swiper-pagination"></div>
 					</div>
 				</el-col>
 			</el-row>
 		</div>
 	</div>
-	<div class="container"
-	     style="height:2000px; background-color:#fff;">
+	<!-- about end -->
+	<!-- products start -->
+	<div class="i-products padding-top-100 padding-bottom-100">
+		<div class="container">
+			<div class="heading text-center"><span style="color:#fff;">WHAT WE DO</span>
+				<h4 style="color:#fff;">我们提供最好的产品</h4>
+				<hr>
+				<p style="color:#fff;">
+					提供高端网站建设一条龙服务！丰富的产品种类，领先的技术实力，为您提供最全面的服务！
+				</p>
+			</div>
+		</div>
+	</div>
+	<!-- products end -->
+	<div class="container" style="height:2000px; background-color:#fff;">
 
 	</div>
 </div>
@@ -104,17 +98,22 @@ export default {
 					prevEl: '.banner .swiper-button-prev'
 				},
 				parallax: true,
+
 			},
 			swiperOption2: {
 				speed: 2000,
-				autoplay: {
-					delay: 3000
-				},
-				effect : 'fade',
-				loop: true
+				// autoplay: {
+				// 	delay: 3000
+				// },
+				effect: 'fade',
+				loop: true,
+				pagination: {
+					el: '.aboutpic .swiper-pagination',
+					clickable: true
+				}
 			},
 			nowTab: 1,
-			img:''
+			img: ''
 			// scrollpage: document.getElementsByClassName( "el-scrollbar__wrap" )[ 0 ]
 		}
 	},
@@ -124,12 +123,13 @@ export default {
 		}
 	},
 	async asyncData( {} ) {
-		return  api.getIndex()
+		return api.getIndex()
 			.then( ( res ) => {
 				return {
 					bannerList: res.data.bannerList,
 					aboutContent: res.data.aboutContent,
-					aboutList: res.data.aboutList
+					aboutList: res.data.aboutList,
+					aboutpic: res.data.aboutpic
 				}
 			} )
 			.catch( ( e ) => {
@@ -142,6 +142,6 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
 	@import "~assets/css/index.scss";
 </style>
